cafd1686568c3fc9c0c643a9b9ebdfb13d882d91
noteDexter <joh23nathan@gmail.com>
2019-09-17 09:28:46 -0300

    Implementação de Usuario em andamento

---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+
+namespace AspCore04.Models
+{
+    public class Usuario{
+        public int UsuarioId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email {get; set;}
+
+        public string Senha {get; set;}
+
+        public bool Logado {get; set;}
+        
+        
+    }
+}
